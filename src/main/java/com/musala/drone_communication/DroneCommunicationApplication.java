d18534032b040b2b1e5e27a11aa7e3c35160f9cb
Dmitrii Krasnov <dv.krasn0v@yandex.ru>
2022-11-11 16:31:12 +0600

    created empty project with spring web and spring data jpa

---
+++ src/main/java/com/musala/drone_communication/DroneCommunicationApplication.java
@@ -0,0 +1,13 @@
+package com.musala.drone_communication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneCommunicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroneCommunicationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/musala/drone_communication/DroneCommunicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musala.drone_communication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneCommunicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
